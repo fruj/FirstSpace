a2ed5459d504f8e37fe4c3c77ecc38b0bdf5773b
KawPuDFHFJm <eremey.mSDKGFKJSHGJDHFilovanov.msc@gmail.com>
2020-09-04 15:50:59 +0300

    jkljn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] MASSIVE = new int[]{ 1,2,3,4,5 };
+        int sumMass= 0;
+        for (int i = 0; i < 5; i++){
+        System.out.println("Element at index " + i + " : "+ MASSIVE[i]);
+        sumMass=sumMass+i; }
+
+
+        for(int i : MASSIVE){
+            System.out.println("Value: " + i);
+        }
     }
 }
